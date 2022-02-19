93b56ebca8eeb81784dfbc220dc48edc6241ef8a
A.K <RMK-10M1@yandex.ru>
2022-02-19 14:21:43 +0300

    Initial commit

---
+++ app/src/main/java/com/ka/mymovie/data/MovieDataBase.java
@@ -0,0 +1,24 @@
+package com.ka.mymovie.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Movie.class, FavouriteMovie.class}, version = 3, exportSchema = false)
+public abstract class MovieDataBase extends RoomDatabase{
+    private static final String DB_NAME = "movies.db";
+    private static MovieDataBase database;
+    private static final Object LOCK = new Object();
+
+    public static MovieDataBase getInstance(Context context){
+        synchronized (LOCK) {
+            if (database == null) {
+                database = Room.databaseBuilder(context, MovieDataBase.class, DB_NAME).fallbackToDestructiveMigration().build();
+            }
+        }
+        return database;
+    }
+    public abstract MovieDao movieDao();
+}
